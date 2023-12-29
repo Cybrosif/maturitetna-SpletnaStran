c2e468917b237c9041e65b826152d13b3ce65d9c
Cybrosif <anej.dolercrep@gmail.com>
2023-12-29 15:50:11 +0100

    neavm

---
+++ maturitetna-NovaTestnaStran/Controllers/UserManagementController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace maturitetna_NovaTestnaStran.Controllers
+{
+    public class UserManagementController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

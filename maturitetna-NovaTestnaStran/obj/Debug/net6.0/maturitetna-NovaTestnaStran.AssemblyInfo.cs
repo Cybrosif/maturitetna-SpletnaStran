7e8f48207c143aab2fdc30a764af6d69fac34cc6
ArPlays <sandisandi709@gmail.com>
2024-03-04 07:26:27 +0100

    Popravek izgleda login form

--- maturitetna-NovaTestnaStran/obj/Debug/net6.0/maturitetna-NovaTestnaStran.AssemblyInfo.cs
+++ maturitetna-NovaTestnaStran/obj/Debug/net6.0/maturitetna-NovaTestnaStran.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("maturitetna-NovaTestnaStran")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9028e57b87b418d4d5fc3397b7af6da4207df05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60b4af977f6234542053444cbe4abc6bb6ca9798")]
 [assembly: System.Reflection.AssemblyProductAttribute("maturitetna-NovaTestnaStran")]
 [assembly: System.Reflection.AssemblyTitleAttribute("maturitetna-NovaTestnaStran")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

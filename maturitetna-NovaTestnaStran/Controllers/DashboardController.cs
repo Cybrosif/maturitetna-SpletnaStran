87dc9a4c611641523fc2980d937f6d245b34f9aa
Cybrosif <anej.dolercrep@gmail.com>
2024-03-24 17:43:44 +0100

    chanfes

--- maturitetna-NovaTestnaStran/Controllers/DashboardController.cs
+++ maturitetna-NovaTestnaStran/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
 using maturitetna_NovaTestnaStran.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,12 +8,14 @@ namespace maturitetna_NovaTestnaStran.Controllers;
 public class DashboardController : Controller
 {
     private readonly ApplicationDbContext _context;
+    private readonly UserManager<IdentityUser> _userManager;
     //public IActionResult Index()
     //{
     //    return View();
     //}
-    public DashboardController(ApplicationDbContext context)
+    public DashboardController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
     {
+        _userManager = userManager;
         _context = context;
     }
     
@@ -20,12 +23,29 @@ public class DashboardController : Controller
     {
         var viewModel = new ModelView();
         
-        string targetDomain = "MojaDomena1";
+        
+        string targetDomain = await getUserDomain();
         
         viewModel.Servers = await _context.Servers
             .Where(server => server.Domain.Domain == targetDomain)
             .ToListAsync();
 
         return View(viewModel);
+    }
+
+    private async Task<string?> getUserDomain()
+    {
+        IdentityUser user = await _userManager.GetUserAsync(User);
+        string userId = user.Id;
+        int domainId = await _context.UserDomain
+            .Where(ud => ud.UserId == userId)
+            .Select(ud => ud.DomainId)
+            .FirstOrDefaultAsync();
+        string domain = await _context.Domain
+            .Where(d => d.Id == domainId)
+            .Select(ud => ud.Domain)
+            .FirstOrDefaultAsync();
+
+        return domain;
     }
 }

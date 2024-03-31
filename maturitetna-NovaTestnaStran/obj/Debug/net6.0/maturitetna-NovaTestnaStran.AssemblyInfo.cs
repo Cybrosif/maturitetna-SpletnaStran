f40884caf058c943d68d995408bf41f0a886966e
ArPlays <sandisandi709@gmail.com>
2024-03-31 12:10:39 +0200

    Bek z cpu/ram zgodovino

--- maturitetna-NovaTestnaStran/obj/Debug/net6.0/maturitetna-NovaTestnaStran.AssemblyInfo.cs
+++ maturitetna-NovaTestnaStran/obj/Debug/net6.0/maturitetna-NovaTestnaStran.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("maturitetna-NovaTestnaStran")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb2c88a984eb26e2a65f1137e6205f9cd9130bae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da98b53300c5da91ed0efa4e1a5d5b2a5c8d9546")]
 [assembly: System.Reflection.AssemblyProductAttribute("maturitetna-NovaTestnaStran")]
 [assembly: System.Reflection.AssemblyTitleAttribute("maturitetna-NovaTestnaStran")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

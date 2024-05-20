13e42d085f11c6b6f4b0f8e20aa06058f147dfcf
ArPlays <sandisandi709@gmail.com>
2024-05-20 20:39:26 +0200

    šiptarija

---
+++ maturitetna-NovaTestnaStran/Controllers/OutagesController .cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace maturitetna_NovaTestnaStran.Controllers
+{
+    public class OutagesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+
+}
--- maturitetna-NovaTestnaStran/obj/Debug/net6.0/maturitetna-NovaTestnaStran.AssemblyInfo.cs
+++ maturitetna-NovaTestnaStran/obj/Debug/net6.0/maturitetna-NovaTestnaStran.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("maturitetna-NovaTestnaStran")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23f5d3d6e4f613c2d4a9b4ef504fd188a23ba33b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e7651b0e29acd3c21276f4c59c7c5df8f12aa42")]
 [assembly: System.Reflection.AssemblyProductAttribute("maturitetna-NovaTestnaStran")]
 [assembly: System.Reflection.AssemblyTitleAttribute("maturitetna-NovaTestnaStran")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
